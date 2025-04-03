582d5ac35e51ed2c04f839ce0d74566b1877faee
long <phan.cheanlong@institute.pse.ngo>
2025-04-03 08:27:50 +0700

    change button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,7 +5,7 @@ type props = {
 };
 const button = ({ label }: props) => {
   return (
-    <button className="bg-[#6A0DAD] text-white font-bold py-2 px-4 rounded-full text-4xl font-[Cinzel]">
+    <button className="bg-[#6A0DAD] text-white font-bold py-2 px-4 rounded-full text-xl font-[Cinzel]">
       {label}
     </button>
   );
