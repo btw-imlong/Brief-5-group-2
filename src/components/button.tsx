e9502844a925402f7164135ecce471995084806e
phea <rith.sophea@institute.pse.ngo>
2025-04-03 07:46:16 +0700

    add more

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const button = () => {
   return (
-    <button className="bg-[#6A0DAD] text-white font-bold py-2 px-4 rounded-full text-4xl">
+    <button className="bg-[#6A0DAD] text-white font-bold py-2 px-4 rounded-full text-4xl font-[Cinzel]">
       Go to favorite
     </button>
   );
