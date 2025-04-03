2bdf3c2ca552c01d635ef58ac5ff2739efd18881
kakadapheap <pheap.kakada@institute.pse.ngo>
2025-04-03 08:34:33 +0700

    mereg footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/navbar";
 import Home from "./page/home";
+import Footer from "./components/footter"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/footter.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className="bg-[#6A0DAD] text-white text-center p-8 mt-3">
+        <div className="container mx-auto flex flex-col md:flex-row justify-center items-center gap-4">
+        © 2025 Fairy Tale Audio. All rights reserved.
+        </div>
+      </footer>
+    );
+  }
